37853b9a301b7219516b9c241c7f2ea9b0bd05c3
gerkone <gianlucagalletti@ymail.com>
2023-05-18 21:30:11 +0200

    encoder with batched nodes

--- src/encoder/egnn.py
+++ src/encoder/egnn.py
@@ -1,9 +1,12 @@
 """Adapted from https://github.com/vgsatorras/egnn"""
 
+from typing import Tuple
+
 import torch
 from torch import nn
 from torch_geometric.nn import knn_graph
-from torch_kmeans import SoftKMeans
+from torch_scatter import scatter_add, scatter_max, scatter_mean
+
 
 class E_GCL(nn.Module):
     """
@@ -70,11 +73,11 @@ class E_GCL(nn.Module):
         if self.attention:
             att_val = self.att_mlp(out)
             out = out * att_val
-        return out
+        return out  # (n_edges, hidden_dim)
 
     def node_model(self, x, edge_index, edge_attr, node_attr):
         src, _ = edge_index
-        agg = unsorted_segment_sum(edge_attr, src, num_segments=x.size(0))
+        agg = scatter_add(edge_attr, src, dim=0)
         if node_attr is not None:
             agg = torch.cat([x, agg, node_attr], dim=1)
         else:
@@ -82,19 +85,25 @@ class E_GCL(nn.Module):
         out = self.node_mlp(agg)
         if self.residual:
             out = x + out
-        return out
+        return out  # (n_nodes, hidden_dim)
 
     def coord_model(self, coord, edge_index, coord_diff, edge_feat):
         src, _ = edge_index
-        trans = coord_diff * self.coord_mlp(edge_feat)
-        if self.coords_agg == "sum":
-            agg = unsorted_segment_sum(trans, src, num_segments=coord.size(0))
-        elif self.coords_agg == "mean":
-            agg = unsorted_segment_mean(trans, src, num_segments=coord.size(0))
-        else:
-            raise Exception("Wrong coords_agg parameter" % self.coords_agg)
+        trans = coord_diff * self.coord_mlp(edge_feat)  # (n_edges, 3)
+        agg = self.coords_agg(trans, src, dim=0)  # (n_nodes, 3)
+        # TODO (GAL) bug somewhere here: edge index is not correct
+        if agg.shape != coord.shape:
+            # TODO remove once figured out
+            # add padding to agg
+            agg = torch.cat(
+                [
+                    agg,
+                    torch.zeros((coord.shape[0] - agg.shape[0], 3), device=agg.device),
+                ],
+                dim=0,
+            )
         coord = coord + agg
-        return coord
+        return coord  # (n_nodes, 3)
 
     def coord2radial(self, edge_index, coord):
         src, dst = edge_index
@@ -109,9 +118,15 @@ class E_GCL(nn.Module):
     def forward(self, h, edge_index, coord, edge_attr=None, node_attr=None):
         src, dst = edge_index
         radial, coord_diff = self.coord2radial(edge_index, coord)
-        edge_feat = self.edge_model(h[src], h[dst], radial, edge_attr)
-        coord = self.coord_model(coord, edge_index, coord_diff, edge_feat)
-        h = self.node_model(h, edge_index, edge_feat, node_attr)
+        edge_feat = self.edge_model(
+            h[src], h[dst], radial, edge_attr
+        )  # (n_edges, hidden_dim)
+        coord = self.coord_model(
+            coord, edge_index, coord_diff, edge_feat
+        )  # (n_nodes, 3)
+        h = self.node_model(
+            h, edge_index, edge_feat, node_attr
+        )  # (n_nodes, hidden_dim)
 
         return h, coord, edge_attr
 
@@ -119,12 +134,13 @@ class E_GCL(nn.Module):
 class EGNNLocal(nn.Module):
     def __init__(
         self,
-        dim=3,
         c_dim=128,
+        vector_c_dim=16,
         hidden_dim=64,
-        padding=0.1,
         scalar_dim=1,
         edge_dim=1,
+        scalar_pooling=scatter_max,
+        vector_pooling=scatter_add,
         device="cpu",
         act_fn=nn.SiLU(),
         n_layers=4,
@@ -132,49 +148,60 @@ class EGNNLocal(nn.Module):
         residual=True,
         attention=False,
         normalize=False,
-        coords_agg="sum",
+        coords_agg=scatter_add,
         tanh=False,
         eps=1e-8,
     ):
         """
-        :param dim: input point dimension
-        :param c_dim: Number of features for 'h' at the output
-        :param hidden_dim: Number of hidden features
-        :param padding: conventional padding paramter of ONet for unit cube, so [-0.5, 0.5] -> [-0.55, 0.55]
-        :param scalar_dim: Number of features for 'h' at the input
-        :param edge_dim: Number of features for the edge features
-        :param device: Device (e.g. 'cpu', 'cuda:0',...)
-        :param act_fn: Non-linearity
-        :param n_layers: Number of layer for the EGNN
-        :param n_neighbors: Number of neighbors to consider in the knn graph
-        :param residual: Use residual connections, we recommend not changing this one
-        :param attention: Whether using attention or not
-        :param normalize: Normalizes the coordinates messages such that:
-                    instead of: x^{l+1}_i = x^{l}_i + Σ(x_i - x_j)phi_x(m_ij)
-                    we get:     x^{l+1}_i = x^{l}_i + Σ(x_i - x_j)phi_x(m_ij)/||x_i - x_j||
-                    We noticed it may help in the stability or generalization in some future works.
-                    We didn't use it in our paper.
-        :param tanh: Sets a tanh activation function at the output of phi_x(m_ij). I.e. it bounds the output of
-                        phi_x(m_ij) which definitely improves in stability but it may decrease in accuracy.
-                        We didn't use it in our paper.
-        :param eps: Small number to avoid numerical instabilities
+        Args:
+            scalar_c_dim (int): Number of features for 'h' at the output
+            vector_c_dim (int): Number of features for 'x' at the output
+            hidden_dim (int): Number of hidden features
+            scalar_dim (int): Number of features for 'h' at the input
+            edge_dim (int): Number of features for the edge features
+            scalar_pooling (str): Pooling function for the scalar features
+            vector_pooling (str): Pooling function for the vector features
+            device (str): Device (e.g. 'cpu', 'cuda:0',...)
+            act_fn (str): Non-linearity
+            n_layers (int): Number of layers for the EGNN
+            n_neighbors (int): Number of neighbors to consider in the knn graph
+            residual (bool): Use residual connections, we recommend not changing this one
+            attention (bool): Whether using attention or not
+            normalize (bool): Normalizes the coordinate messages such that:
+            tanh (bool): Sets a tanh activation function at the output of phi_x(m_ij).
+            eps (float): Small number to avoid numerical instabilities
         """
         super().__init__()
-        _ = dim, padding
         self.hidden_dim = hidden_dim
         self.device = device
         self.n_layers = n_layers
         self.n_neighbors = n_neighbors
         self.eps = eps
-        # TODO should not be fixed
-        self.n_objects = 5
+
+        assert coords_agg in [scatter_add, scatter_mean]
+        assert scalar_pooling in [scatter_add, scatter_mean, scatter_max]
+        assert vector_pooling in [scatter_add, scatter_mean]
+        self.scalar_pooling = scalar_pooling
+        if self.scalar_pooling == scatter_max:
+            self.scalar_pooling = lambda *args, **kw: scatter_max(*args, **kw)[0]
+        self.vector_pooling = vector_pooling
 
         self.embedding_in = nn.Linear(scalar_dim, self.hidden_dim)
-        self.scalar_embedding_out = nn.Linear(self.hidden_dim, c_dim)
-        self.vector_embedding_out = nn.Linear(1, c_dim)
+
+        self.c_dim = c_dim
+        self.vector_c_dim = vector_c_dim
+
+        # readout
+        self.scalar_readout = nn.Linear(self.hidden_dim, c_dim)
+        self.readout_mix_net = nn.Sequential(
+            nn.Linear(self.hidden_dim + 1, self.hidden_dim),
+            act_fn,
+            nn.Linear(self.hidden_dim, c_dim + vector_c_dim),
+        )
+
         for i in range(0, n_layers):
             self.add_module(
-                "gcl_%d" % i,
+                f"gcl_{i}",
                 E_GCL(
                     self.hidden_dim,
                     self.hidden_dim,
@@ -190,32 +217,32 @@ class EGNNLocal(nn.Module):
             )
         self.to(self.device)
 
-    def forward(self, pc, ):
-        bs, n_nodes, _ = pc.shape
+    def forward(self, inputs: Tuple[torch.Tensor, torch.Tensor]):
+        """Encoder forward pass
+
+        Args:
+            inputs (tuple): point cloud and number of nodes per graph
+
+        Returns:
+            Tuple with the object codes and batch indices per-node
+        """
+        pc, n_nodes = inputs  # (n_nodes, 3), (bs,)
         # feature transform
-        h, x, edges, edge_attr = self._transform(pc)
+        h, x, edges, edge_attr, batch = self._transform(pc, n_nodes)
         # embedding
-        h = self.embedding_in(h)
+        h = self.embedding_in(h)  # (n_nodes, hidden_dim)
         # message passing
         for i in range(0, self.n_layers):
-            h, x, _ = self._modules["gcl_%d" % i](h, edges, x, edge_attr=edge_attr)
-        # outputs
-        scalar_node_embeddings = self.scalar_embedding_out(h).view(bs, n_nodes, -1)
-        gate = self.vector_embedding_out(torch.norm(x, dim=-1, keepdim=True))[..., None]
-        vector_node_embeddings = (x[:, None] * gate).view(bs, n_nodes, -1)
-        node_embeddings = (scalar_node_embeddings, vector_node_embeddings)
-        node_tag = SoftKMeans(
-            init_method="k-means++", n_clusters=self.n_objects, verbose=False
-        )(scalar_node_embeddings).labels
-        return node_embeddings, node_tag
-    
-    def _transform(self, pc):
+            h, x, _ = self._modules[f"gcl_{i}"](h, edges, x, edge_attr=edge_attr)
+
+        s_codes, v_codes = self._readout(h, x, batch)
+        return s_codes, v_codes
+
+    def _transform(self, pc, n_nodes):
         # edge indices (knn)
-        bs, n_nodes, _ = pc.shape
-        # flatten batch and nodes
-        pc = pc.view(bs * n_nodes, -1)
-        batch = torch.arange(0, bs, device=pc.device)
-        batch = batch.repeat_interleave(n_nodes).long()
+        bs = n_nodes.shape[0]
+        # batch index per node (avoid knn across graphs)
+        batch = torch.arange(0, bs, device=pc.device).repeat_interleave(n_nodes).long()
         edges = knn_graph(pc, self.n_neighbors, batch)
         # scalar node features (norms)
         h = torch.norm(pc, dim=-1, keepdim=True)
@@ -223,23 +250,21 @@ class EGNNLocal(nn.Module):
         x = pc
         # edge features (distance)
         edge_attr = torch.norm(x[edges[0]] - x[edges[1]], dim=-1, keepdim=True)
-        return h, x, edges, edge_attr
+        return h, x, edges, edge_attr, batch
 
+    def _readout(self, h, x, batch):
+        # scalar readout
+        s_codes = self.scalar_pooling(h, batch, dim=0)  # (bs, hidden_dim)
+        v_codes = self.vector_pooling(x, batch, dim=0)  # (bs, 3)
 
-# TODO replace these with torch_scatter
-def unsorted_segment_sum(data, segment_ids, num_segments):
-    result_shape = (num_segments, data.size(1))
-    result = data.new_full(result_shape, 0)  # Init empty result tensor.
-    segment_ids = segment_ids.unsqueeze(-1).expand(-1, data.size(1))
-    result.scatter_add_(0, segment_ids, data)
-    return result
+        mix = self.readout_mix_net(
+            torch.cat([s_codes, torch.norm(v_codes, dim=-1, keepdim=True)], dim=-1)
+        )  # (bs, c_dim + vector_c_dim)
+        scalar_mix, vector_mix = torch.split(
+            mix, [self.c_dim, self.vector_c_dim], dim=-1
+        )
+        s_codes = self.scalar_readout(s_codes) * scalar_mix  # (n_nodes, c_dim)
+        # vector readout
+        v_codes = v_codes[:, None] * vector_mix[..., None]  # (n_nodes, vector_c_dim, 3)
 
-
-def unsorted_segment_mean(data, segment_ids, num_segments):
-    result_shape = (num_segments, data.size(1))
-    segment_ids = segment_ids.unsqueeze(-1).expand(-1, data.size(1))
-    result = data.new_full(result_shape, 0)  # Init empty result tensor.
-    count = data.new_full(result_shape, 0)
-    result.scatter_add_(0, segment_ids, data)
-    count.scatter_add_(0, segment_ids, torch.ones_like(data))
-    return result / count.clamp(min=1)
+        return s_codes, v_codes
