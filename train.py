79410801e78c3ab9ec2941cc4aa63affa3bddd93
gerkone <gianlucagalletti@ymail.com>
2023-07-04 22:36:03 +0200

    new train and test loop

--- train.py
+++ train.py
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "--new", action="store_true", help="Ignore checkpoints, start new training."
     )
+    parser.add_argument("--test", action="store_true", help="Run test evaulation.")
     parser.add_argument("--no-cuda", action="store_true", help="Do not use cuda.")
     parser.add_argument(
         "--exit-after",
@@ -35,6 +36,7 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
     cfg = config.load_config(args.config, "configs/default.yaml")
+
     is_cuda = torch.cuda.is_available() and not args.no_cuda
     device = torch.device("cuda" if is_cuda else "cpu")
     # Set t0
@@ -57,8 +59,7 @@ if __name__ == "__main__":
         raise ValueError("model_selection_mode must be " "either maximize or minimize.")
 
     # Output directory
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
+    os.makedirs(out_dir, exist_ok=True)
 
     shutil.copyfile(args.config, os.path.join(out_dir, "config.yaml"))
 
@@ -84,8 +85,20 @@ if __name__ == "__main__":
         worker_init_fn=data.worker_init_fn,
     )
 
-    # TODO put back in
+    if args.test:
+        test_dataset = config.get_dataset("test", cfg, return_idx=True)
+        test_loader = torch.utils.data.DataLoader(
+            test_dataset,
+            batch_size=1,
+            num_workers=cfg["training"]["n_workers_val"],
+            shuffle=False,
+            collate_fn=data.collate_remove_none,
+            worker_init_fn=data.worker_init_fn,
+        )
+
     # For visualizations
+    val_dataset = config.get_dataset("test", cfg, return_idx=True)
+
     vis_loader = torch.utils.data.DataLoader(
         val_dataset,
         batch_size=1,
@@ -125,7 +138,6 @@ if __name__ == "__main__":
     # Intialize training
     optimizer = optim.Adam(model.parameters(), lr=cfg["training"]["learning_rate"])
     scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.85)
-    # optimizer = optim.SGD(model.parameters(), lr=1e-4, momentum=0.9)
     trainer = config.get_trainer(model, optimizer, cfg, device=device)
 
     checkpoint_io = CheckpointIO(out_dir, model=model, optimizer=optimizer)
@@ -162,127 +174,159 @@ if __name__ == "__main__":
 
     print(f"output path: {cfg['training']['out_dir']}")
 
-    losses = []
-    bss = []
-    while True:
-        epoch_it += 1
-        try:
-            train_iter = iter(train_loader)
-            for batch in train_iter:
-                it += 1
+    if not args.test:
+        print("Training")
+        losses = []
+        bss = []
+        while True:
+            epoch_it += 1
+            try:
+                train_iter = iter(train_loader)
+                for batch in train_iter:
+                    it += 1
 
-                if sequential_batches > 1:
-                    batches = [batch]
-                    try:
-                        for _ in range(sequential_batches - 1):
-                            batches.append(next(train_iter))
-                    except StopIteration:
-                        pass
-                else:
-                    batches = batch
-
-                out = trainer.train_step(batches)
-                if isinstance(out, tuple):
-                    loss, actual_bs = out
-                else:
-                    loss, actual_bs = out, batch_size
-
-                if loss is not None:
-                    logger.add_scalar("train/loss", loss, it)
-                    losses.append(loss)
-                if actual_bs is not None:
-                    bss.append(actual_bs)
-
-                # Print output
-                if print_every > 0 and (it % print_every) == 0:
-                    t = datetime.datetime.now()
-                    since = time.strftime(
-                        "%H:%M:%S", time.gmtime((t - t0).total_seconds())
-                    )
-                    loss = sum(losses) / len(losses)
-                    avg_bs = round(sum(bss) / len(bss))
-                    losses = []
-                    bss = []
-                    print(
-                        f"[Epoch {epoch_it}] it={it}, "
-                        f"train loss={loss:.3f}, time={since}, "
-                        f"actual/real batch={avg_bs}/{batch_size}"
-                    )
-
-                # Visualize output
-                if visualize_every > 0 and (it % visualize_every) == 0:
-                    print("Visualizing")
-                    for data_vis in data_vis_list:
-                        if cfg["generation"]["sliding_window"]:
-                            out = generator.generate_mesh_sliding(data_vis["data"])
-                        else:
-                            out = generator.generate_mesh(data_vis["data"])
-                        # Get statistics
+                    if sequential_batches > 1:
+                        batches = [batch]
                         try:
-                            mesh, stats_dict = out
-                        except TypeError:
-                            mesh, stats_dict = out, {}
+                            for _ in range(sequential_batches - 1):
+                                batches.append(next(train_iter))
+                        except StopIteration:
+                            pass
+                    else:
+                        batches = batch
 
-                        mesh.export(
-                            os.path.join(
-                                out_dir,
-                                "vis",
-                                f"{it}_{data_vis['category']}_{data_vis['it']}.obj",
-                            )
+                    out = trainer.train_step(batches)
+                    if isinstance(out, tuple):
+                        loss, actual_bs = out
+                    else:
+                        loss, actual_bs = out, batch_size
+
+                    if loss is not None:
+                        logger.add_scalar("train/loss", loss, it)
+                        losses.append(loss)
+                    if actual_bs is not None:
+                        bss.append(actual_bs)
+
+                    # Print output
+                    if print_every > 0 and (it % print_every) == 0:
+                        t = datetime.datetime.now()
+                        since = time.strftime(
+                            "%H:%M:%S", time.gmtime((t - t0).total_seconds())
+                        )
+                        loss = sum(losses) / len(losses)
+                        avg_bs = round(sum(bss) / len(bss))
+                        losses = []
+                        bss = []
+                        print(
+                            f"[Epoch {epoch_it}] it={it}, "
+                            f"train loss={loss:.3f}, time={since}, "
+                            f"actual/real batch={avg_bs}/{batch_size}"
                         )
 
-                # Save checkpoint
-                if checkpoint_every > 0 and (it % checkpoint_every) == 0:
-                    print("Saving checkpoint")
-                    checkpoint_io.save(
-                        "model.pt",
-                        epoch_it=epoch_it,
-                        it=it,
-                        loss_val_best=metric_val_best,
-                    )
+                    # Visualize output
+                    if visualize_every > 0 and (it % visualize_every) == 0:
+                        print("Visualizing")
+                        for data_vis in data_vis_list:
+                            if cfg["generation"]["sliding_window"]:
+                                out = generator.generate_mesh_sliding(data_vis["data"])
+                            else:
+                                out = generator.generate_mesh(data_vis["data"])
+                            # Get statistics
+                            try:
+                                mesh, stats_dict = out
+                            except TypeError:
+                                mesh, stats_dict = out, {}
 
-                # Backup if necessary
-                if backup_every > 0 and (it % backup_every) == 0:
-                    print("Backup checkpoint")
-                    checkpoint_io.save(
-                        f"model_{it}.pt",
-                        epoch_it=epoch_it,
-                        it=it,
-                        loss_val_best=metric_val_best,
-                    )
-                # Run validation
-                if validate_every > 0 and (it % validate_every) == 0:
-                    eval_dict = trainer.evaluate(val_loader)
-                    metric_val = eval_dict[model_selection_metric]
-                    print(f"Model selection ({model_selection_metric}): {metric_val}")
+                            if isinstance(mesh, tuple):
+                                mesh, _ = mesh
 
-                    print("Validation results:", eval_dict)
+                            mesh.export(
+                                os.path.join(
+                                    out_dir,
+                                    "vis",
+                                    f"{it}_{data_vis['category']}_{data_vis['it']}.obj",
+                                )
+                            )
 
-                    for k, v in eval_dict.items():
-                        logger.add_scalar(f"val/{k}", v, it)
-
-                    if model_selection_sign * (metric_val - metric_val_best) > 0:
-                        metric_val_best = metric_val
-                        print(f"New best model (loss {metric_val_best})")
+                    # Save checkpoint
+                    if checkpoint_every > 0 and (it % checkpoint_every) == 0:
+                        print("Saving checkpoint")
                         checkpoint_io.save(
-                            "model_best.pt",
+                            "model.pt",
                             epoch_it=epoch_it,
                             it=it,
                             loss_val_best=metric_val_best,
                         )
 
-                # Exit if necessary
-                if exit_after > 0 and (datetime.datetime.now() - t0) >= exit_after:
-                    print("Time limit reached. Exiting.")
-                    checkpoint_io.save(
-                        "model.pt",
-                        epoch_it=epoch_it,
-                        it=it,
-                        loss_val_best=metric_val_best,
-                    )
-                    exit(3)
-            if scheduler.get_last_lr()[0] > 5e-5:
-                scheduler.step()
-        except Exception as e:
-            print(f"ERROR (main) {e.__doc__}: {e}")
-            pass
+                    # Backup if necessary
+                    if backup_every > 0 and (it % backup_every) == 0:
+                        print("Backup checkpoint")
+                        checkpoint_io.save(
+                            f"model_{it}.pt",
+                            epoch_it=epoch_it,
+                            it=it,
+                            loss_val_best=metric_val_best,
+                        )
+                    # Run validation
+                    if validate_every > 0 and (it % validate_every) == 0:
+                        eval_dict = trainer.evaluate(val_loader)
+                        metric_val = eval_dict[model_selection_metric]
+                        print(
+                            f"Model selection ({model_selection_metric}): {metric_val}"
+                        )
+
+                        print("Validation results:", eval_dict)
+
+                        for k, v in eval_dict.items():
+                            logger.add_scalar(f"val/{k}", v, it)
+
+                        if model_selection_sign * (metric_val - metric_val_best) > 0:
+                            metric_val_best = metric_val
+                            print(f"New best model (loss {metric_val_best})")
+                            checkpoint_io.save(
+                                "model_best.pt",
+                                epoch_it=epoch_it,
+                                it=it,
+                                loss_val_best=metric_val_best,
+                            )
+
+                    # Exit if necessary
+                    if exit_after > 0 and (datetime.datetime.now() - t0) >= exit_after:
+                        print("Time limit reached. Exiting.")
+                        checkpoint_io.save(
+                            "model.pt",
+                            epoch_it=epoch_it,
+                            it=it,
+                            loss_val_best=metric_val_best,
+                        )
+                        exit(3)
+                if scheduler.get_last_lr()[0] > 5e-5:
+                    scheduler.step()
+            except Exception as e:
+                print(f"ERROR (main) {e.__doc__}: {e}")
+                pass
+    else:
+        print("Testing")
+        eval_dict = trainer.evaluate(val_loader)
+        metric_val = eval_dict[model_selection_metric]
+        print("Test results:", eval_dict)
+        print("Visualizing")
+        for data_vis in data_vis_list:
+            if cfg["generation"]["sliding_window"]:
+                out = generator.generate_mesh_sliding(data_vis["data"])
+            else:
+                out = generator.generate_mesh(data_vis["data"])
+            # Get statistics
+            try:
+                mesh, stats_dict = out
+            except TypeError:
+                mesh, stats_dict = out, {}
+
+            mesh.export(
+                os.path.join(
+                    out_dir,
+                    "vis",
+                    f"test_vis_{data_vis['category']}_{data_vis['it']}.obj",
+                )
+            )
+        exit()
